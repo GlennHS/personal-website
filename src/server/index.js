285a775d1ad7be59d348b7dfd9736d6dce186409
GlennHS <glenniumhs@gmail.com>
2018-08-17 22:56:07 +0100

    Website base complete with mobile compatability

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,79 @@
 M.AutoInit();
+
+let userHasScrolled = false;
+/**
+ * @fires onPageLoad
+ * 
+ * No arguments
+ * Colors the nav button of the current page
+ */
+const colorNavButton = () => {
+  let buttonToChange = '';
+  switch(window.location.href.substr(window.location.href.length - 10)) {
+    case 'index.html':
+      buttonToChange = document.getElementById('nav-home');
+      break;
+    case 'about.html':
+      buttonToChange = document.getElementById('nav-about');
+      break;
+    case 'jects.html':
+      buttonToChange = document.getElementById('nav-projects');
+      break;
+    case 'links.html':
+      buttonToChange = document.getElementById('nav-links');
+      break;
+  }
+  buttonToChange.classList.remove('teal', 'lighten-1');
+  buttonToChange.classList.add('teal', 'lighten-3');
+}
+
+/**
+ * @fires onPageLoad
+ * 
+ * Randomizes the colors of any element with color random-color
+ */
+const randomizeColors = () => {
+  const elementArray = Array.from(document.getElementsByClassName('random-color'));
+  elementArray.forEach( item => {
+    // Make a blank array to store RGB
+    let newColor = [];
+    // Generate Red Green and Blue values
+    for(i=0;i<3;i++) {
+      newColor.push(Math.floor(Math.random() * 172))
+    }
+    // Stringify the array and apply the new color to the object
+    // Colors always look like "rgb(r, g, b)"
+    const colorString = `rgb(${newColor[0]}, ${newColor[1]}, ${newColor[2]})`
+    item.style.color = colorString;
+  })
+}
+
+// Cool little go to top function
+const goToTop = () => {
+  scroll(0,0);
+  const btntop = document.getElementById('btn-top');
+  btntop.classList.remove('show');
+  btntop.classList.add('hide');
+  window.setTimeout(() => {userHasScrolled = false}, 500);
+}
+
+// Perform onload functions
+window.onload = () => {
+  colorNavButton();
+  randomizeColors();
+}
+
+window.addEventListener('scroll', () => {
+  if (!userHasScrolled) {
+    userHasScrolled = true;
+    const btntop = document.getElementById('btn-top');
+    btntop.classList.remove('hide');
+    btntop.classList.add('show', 'pulse');
+    window.setTimeout(() => {btntop.classList.remove('pulse')},3000);
+  }
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
